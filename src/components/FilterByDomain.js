925098ac6e51d9fd18d0bc4bafbf289721132a55
sirrajeevgupta <rajeevsofficials@gmail.com>
2025-02-10 19:33:51 +0530

    css improvement

--- src/components/FilterByDomain.js
+++ src/components/FilterByDomain.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const FilterByDomain = () => {
-  return <section>FilterByDomain</section>;
+  return (
+    <section>
+      <ul className='domains'>
+        <li>
+          <Link>JavaScript</Link>
+        </li>
+        <li>
+          <Link>ReactJs</Link>
+        </li>
+        <li>
+          <Link>NodeJs</Link>
+        </li>
+        <li>
+          <Link>HTML</Link>
+        </li>
+        <li>
+          <Link>CSS</Link>
+        </li>
+        <li>
+          <Link>SASS</Link>
+        </li>
+        <li>
+          <Link>Miscellaneous</Link>
+        </li>
+      </ul>
+    </section>
+  );
 };
 
 export default FilterByDomain;
