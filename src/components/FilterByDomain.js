cf172c76b579465b6959c92ee5cff6ae4cdc3cba
sirrajeevgupta <rajeevsofficials@gmail.com>
2025-02-11 22:33:01 +0530

    added cube

--- src/components/FilterByDomain.js
+++ src/components/FilterByDomain.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faNode,
+  faHtml5,
+  faCss3,
+  faReact,
+  faJsSquare,
+  faSass,
+} from '@fortawesome/free-brands-svg-icons';
 
 const FilterByDomain = () => {
   return (
-    <section>
+    <section className='domainsPage'>
       <ul className='domains'>
         <li>
           <Link to='/domains/JavaScript'>JavaScript</Link>
@@ -27,6 +36,28 @@ const FilterByDomain = () => {
           <Link to='/domains/Miscellaneous'>Miscellaneous</Link>
         </li>
       </ul>
+      <div className='stage-cube-cont'>
+        <div className='cubespinner'>
+          <div className='face1'>
+            <FontAwesomeIcon icon={faNode} color='#4f9b43' />
+          </div>
+          <div className='face2'>
+            <FontAwesomeIcon icon={faHtml5} color='#F06529' />
+          </div>
+          <div className='face3'>
+            <FontAwesomeIcon icon={faCss3} color='#28A4D9' />
+          </div>
+          <div className='face4'>
+            <FontAwesomeIcon icon={faReact} color='#5ED4F4' />
+          </div>
+          <div className='face5'>
+            <FontAwesomeIcon icon={faJsSquare} color='#EFD81D' />
+          </div>
+          <div className='face6'>
+            <FontAwesomeIcon icon={faSass} color='#de0a17' />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
